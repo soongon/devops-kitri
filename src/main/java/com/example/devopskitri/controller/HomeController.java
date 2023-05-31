6e1e7c4990e09410e42961b165864b1451a374c8
kitri <kitri@gmail.com>
2023-05-31 14:31:02 +0900

    변경

--- src/main/java/com/example/devopskitri/controller/HomeController.java
+++ src/main/java/com/example/devopskitri/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/home")
     public String home() {
-        return "안녕 데브옵스...hi..";
+        return "안녕 데브옵스...hi..sdfdsfsdfs";
     }
 }
