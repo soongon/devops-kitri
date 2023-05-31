c0d8fd7fbf6a8a2865d67db1e559b88eb1ab83bf
kitri <kitri@gmail.com>
2023-05-31 13:32:20 +0900

    변경

--- src/main/java/com/example/devopskitri/DevopsKitriApplication.java
+++ src/main/java/com/example/devopskitri/DevopsKitriApplication.java
@@ -2,9 +2,10 @@ package com.example.devopskitri;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DevopsKitriApplication {
+public class DevopsKitriApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsKitriApplication.class, args);
