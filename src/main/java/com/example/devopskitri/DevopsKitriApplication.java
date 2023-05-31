ab9fa66c9f07ce2f159c7bb85e9c388de1648afe
kitri <kitri@gmail.com>
2023-05-31 10:09:06 +0900

    initial commit

---
+++ src/main/java/com/example/devopskitri/DevopsKitriApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopskitri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsKitriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsKitriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devopskitri/DevopsKitriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopskitri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsKitriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
